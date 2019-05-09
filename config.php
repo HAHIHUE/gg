ebe46ad2e8e9f5c42932a8f73c465ad6f95c2858
HAHIHUE <50430993+HAHIHUE@users.noreply.github.com>
2019-05-09 09:42:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$email = "inijimel06@gmail.com";
+$passcode = "6ZLK1";
+$device_id = "4cccdd1d7ad9a160";
+$push_token = 
+"cWgI_4AcUmw:APA91bEPJFgaDIj143ndLFRmyWc7SVUmvLpKV0oexZO1i3l5Jpe7DYX-FBPNOTgwU4VAbCloTpNNlw26fk2clNCU5d5nLkGvkjF4DMiAnCt2gPzANKKHCESSH2mKy9Gk-umOyzF6UN9t";
